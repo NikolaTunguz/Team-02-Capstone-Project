f82765ece6a6766e69b94f8124010423f2b9363b
Reni Wu <reniw@nevada.unr.edu>
2024-11-02 13:24:06 -0700

    update route

--- Website/web/src/Router.jsx
+++ Website/web/src/Router.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login"; 
+import Login from "./pages/Login";
 import NotFound from './pages/NotFound';
 import Dashboard from './pages/dashboard/index';
-import Notifications from './pages/notifications/index'; 
+import Notifications from './pages/notifications/index';
 import Account from './pages/account/index'
 import Register from './pages/Register';
 
@@ -10,33 +10,33 @@ const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route 
-                    path="/login" 
-                    element={<Login/>} 
+                <Route
+                    path="/login"
+                    element={<Login />}
                 />
-                <Route 
-                    path="/register" 
-                    element={<Register/>} 
+                <Route
+                    path="/register"
+                    element={<Register />}
                 />
-                <Route 
-                    path="*" 
-                    element={<Login/>}
+                <Route
+                    path="/"
+                    element={<Login />}
                 />
-                <Route 
-                    path="*" 
-                    element={<NotFound/>}
+                <Route
+                    path="*"
+                    element={<NotFound />}
                 />
-                <Route 
-                    path="/dashboard" 
-                    element={<Dashboard/>}
+                <Route
+                    path="/dashboard"
+                    element={<Dashboard />}
                 />
-                <Route 
-                    path="/notifications" 
-                    element={<Notifications/>} 
+                <Route
+                    path="/notifications"
+                    element={<Notifications />}
                 />
-                <Route 
-                    path="/account" 
-                    element={<Account/>} 
+                <Route
+                    path="/account"
+                    element={<Account />}
                 />
             </Routes>
         </BrowserRouter>
