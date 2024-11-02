5847c8dabcbe6dbab6aa98992a4fb23d5c12080c
Reni Wu <reniw@nevada.unr.edu>
2024-11-01 23:00:29 -0700

    update livestream component

--- Website/web/src/components/LiveStream.jsx
+++ Website/web/src/components/LiveStream.jsx
@@ -2,44 +2,10 @@ import React, { useEffect, useRef } from 'react';
 import '../App.css';
 
 const LiveStream = () => {
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-    const startStream = async () => {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-        if (videoRef.current) {
-          videoRef.current.srcObject = stream;
-        }
-      } catch (error) {
-        console.error("Error accessing the camera:", error);
-      }
-    };
-
-    startStream();
-
-    return () => {
-      if (videoRef.current && videoRef.current.srcObject) {
-        let stream = videoRef.current.srcObject;
-        let tracks = stream.getTracks();
-
-        tracks.forEach((track) => {
-          track.stop();
-        });
-        videoRef.current.srcObject = null;
-      }
-    };
-  }, []);
-
   return (
-    <>
-      <h3 className="live-stream-title">Live Feed</h3>
-      <div className="live-stream-container">
-        <div className="video-wrapper">
-          <video ref={videoRef} autoPlay playsInline className="video-stream" />
-        </div>
-      </div>
-    </>
+    <div>
+      <h3>Live Stream</h3>
+    </div>
   );
 };
 
