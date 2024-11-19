542abfd8c53c3cb75980c3feec5e2c356b38968a
Nikola Tunguz <tunguznikola@gmail.com>
2024-11-19 15:49:05 -0800

    Template for using thermal models
    Created ThermalModelInterface.py file, will utilize the developed models

---
+++ Models/ThermalModelInterface.py
@@ -0,0 +1,16 @@
+#ConcealedPistolDetection.ipynb is for developing the model.
+#This file (ThermalInterface.py) is for using the model(s).
+
+
+class ThermalInterface():
+    def __init__():
+        pass
+    
+    def process_image():
+        pass
+
+    def detect_pistol():
+        pass
+
+
+
