fc9bda9c9ddff739bfd7f07c59eae538e77bafb3
borne-diego <dborne@nevada.unr.edu>
2024-11-23 21:42:08 -0800

    Initial attempts at threading embedded code.

--- Embedded System/api/venv/main.py
+++ Embedded System/api/venv/main.py
@@ -3,21 +3,63 @@ from flask_cors import CORS
 import cv2
 from datetime import datetime
 import requests
+import sys
+import threading
+from pathlib import Path
+
+base_path = Path(__file__).resolve().parents[3]
+print(base_path)
+sys.path.append(str(base_path))
+
+from Models.model_interface import ModelInterface
 
 app = Flask(__name__)
 cors = CORS(app, origins="*")
+
+
 camera = cv2.VideoCapture(0)
+running = True
+frame = None
+
+def capture_frames():
+    global frame
+    while running:
+        ret, currFrame = camera.read()
+        if not ret:
+            print("BROKEN")
+            break
+        frame = currFrame
+
+
+def process_frames():
+    while running:
+    #     if frame == None():
+    #         print("NO IMAGE")
+    #     else:
+            # model_interface = ModelInterface()
+            # model_interface.set_normal_image("test/test_image.jpg")
+            # # model_interface.setThermalImages("C:\Users\Diego\Pictures\Screenshots\Screenshot 2024-04-03 161659.png")
+            # print("model: ", model_interface.detect_person())
+            # model_interface.detect_pistol()
+            pass
+
+
+
 
 def generate_frames():
+    global frame
     while True:
-        ret, frame = camera.read()
-        if not ret:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        # ret, frame = camera.read()
+        # if not ret:
+        #     break
+        # else:
+
+        if frame is None:
+            continue
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route('/video_feed')
@@ -25,6 +67,21 @@ def video_feed():
     return Response(generate_frames(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+# if __name__ == '__main__':
+#     app.run(port=5000, debug=True)
+
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
-    
+    capture_thread = threading.Thread(target=capture_frames)
+    processing_thread = threading.Thread(target=process_frames)
+
+
+    capture_thread.start()
+    processing_thread.start()
+
+    try: 
+        app.run(port=5000, debug=True)
+    finally:
+        running = False
+        capture_thread.join()
+        processing_thread.join()
+        camera.release()    
