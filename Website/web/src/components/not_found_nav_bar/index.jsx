85c04ea381e8ab38e3c864c3e8bb095b2d1760dc
Reni Wu <reniw@nevada.unr.edu>
2024-12-04 23:40:57 -0800

    not found nav bar updates

--- Website/web/src/components/not_found_nav_bar/index.jsx
+++ Website/web/src/components/not_found_nav_bar/index.jsx
@@ -6,12 +6,9 @@ import templogo from '../../assets/images/templogo.png';
 const NotFoundNavBar = () => {
   return (
     <nav className = 'not_found_nav'>
-      <div className = 'not_found_nav_logo'>
-        <h1> SeeThru </h1>
-        <img 
-          alt = 'logo'
-          src = {templogo}
-        />
+      <div style={{ display: 'flex', alignItems: 'center', gap: '5px' }}>
+        <h1>SeeThru</h1>
+        <img alt="logo" src={templogo} style={{ width: "55px" }} />
       </div>
       <div className = 'not_found_nav_links'>
         <Link to = '/' className = 'not_found_nav_button'> Home </Link>
