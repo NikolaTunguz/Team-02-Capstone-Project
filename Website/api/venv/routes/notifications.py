67e75af17643d19976370ad038bce6dd80724e39
borne-diego <dborne@unr.edu>
2024-12-05 23:07:49 -0800

    Allowed for deletion of notifications.

--- Website/api/venv/routes/notifications.py
+++ Website/api/venv/routes/notifications.py
@@ -25,7 +25,7 @@ def database():
 def notifications():
     user_id = session.get("user_id")
     query = select(
-        Notification.timestamp, Notification.message
+        Notification.timestamp, Notification.message, Notification.device_id
     ).select_from(UserCameras).join(
             Notification, UserCameras.device_id==Notification.device_id
     ).filter(
@@ -37,8 +37,19 @@ def notifications():
     notifications = db.session.execute(query).all()
 
     notifications = [
-        {"timestamp": str(notification[0]), "message": notification[1]}
+        {"timestamp": str(notification[0]), "message": notification[1], "device_id": notification[2]}
         for notification in notifications
     ]
 
     return json.dumps(notifications, default=str)
+
+@notifications_bp.route('/remove_notification', methods=["POST"])
+def remove_notification():
+    device_id = request.get_json().get("device_id")
+    timestamp = request.get_json().get("timestamp")
+
+    notification = Notification.query.filter_by(device_id=device_id, timestamp=timestamp).first()
+    print(notification)
+    db.session.delete(notification)
+    db.session.commit()
+    return '', 200
