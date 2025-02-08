2da74a2af751b72eb457f023b2b251d21c5b78b1
joel-molina <lvmolinajoel@gmail.com>
2025-02-07 19:25:47 -0800

    camera dashboard updates

--- Website/web/src/pages/cameras/index.jsx
+++ Website/web/src/pages/cameras/index.jsx
@@ -1,16 +1,19 @@
+//React imports
 import React from "react";
 import LiveStream from "../../components/LiveStream.jsx";
 import "./index.css";
 import httpClient from "../httpClient";
 import AddCamera from "../../components/AddCamera.jsx";
 import DeleteCamera from "../../components/DeleteCamera.jsx";
-import { Typography, Box, IconButton } from "@mui/material";
+import { Typography, Box, IconButton, Switch } from "@mui/material";
 import { Delete } from "@mui/icons-material";
 
+//React hooks
 export default function Cameras() {
     const [cameras, setCameras] = React.useState([]);
     const [open, setOpen] = React.useState(false);
     const [cameraToDelete, setCameraToDelete] = React.useState(null);
+    const [cameraToggleSwitch, setCameraSwitchState] = React.useState({}); //True & False, no null in a switch.
 
     const getCameras = async () => {
         try {
@@ -21,20 +24,33 @@ export default function Cameras() {
         }
     };
 
+    //handle switch toggle
+    const handleToggle = (camera, cameraNum) => {
+        setCameraSwitchState((prev) => ({
+            ...prev,
+            [`${camera.device_id || cameraNum}`]: !prev[`${camera.device_id || cameraNum}`],
+        }));
+        setOpen(true);
+    };
+
+    //handle delete camera
     const handleDelete = (camera, cameraNum) => {
         setCameraToDelete({camera: camera, cameraNum: cameraNum});
         setOpen(true);
     };
 
+    //closes the add camera modal
     const handleCloseModal = () => {
         setOpen(false);
         setCameraToDelete(null);
     };
 
+    //initial page load function
     React.useEffect(() => {
         getCameras();
     }, []);
 
+    //React component to be returned.
     return (
         <>
             <Box
@@ -45,7 +61,7 @@ export default function Cameras() {
                 marginRight="40px"
             >
                 <h2>Camera Feeds</h2>
-                <AddCamera onCameraAdded={getCameras} />
+                <AddCamera onCameraAdded={getCameras}/>
             </Box>
             
             <div className="camera-container">
@@ -66,8 +82,14 @@ export default function Cameras() {
                             >
                                 <Delete />
                             </IconButton>
+                            <Switch
+                                checked={cameraToggleSwitch?.[camera.device_id || index] || false}
+                                onChange={() => handleToggle(camera, index)}
+                                color="primary"
+                                className="switch-icon"
+                            />
                         {/* </Box> */}
-                        <LiveStream camera={camera} />
+                        <LiveStream camera={camera} className="camera-display"/>
                     </div>
                 ))}
             </div>
@@ -81,6 +103,5 @@ export default function Cameras() {
             )}
         </div>
         </>
-        
     );
 }
