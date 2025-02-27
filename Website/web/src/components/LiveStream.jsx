1e1ec604466ca3f1358b34f1583f4841f698110a
borne-diego <dborne@unr.edu>
2025-02-26 21:56:03 -0800

    Stream testing web.

--- Website/web/src/components/LiveStream.jsx
+++ Website/web/src/components/LiveStream.jsx
@@ -10,17 +10,92 @@ const LiveStream = ({ camera }) => {
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
+    const signalingSocket = new WebSocket("ws://192.168.1.68:8765");
+
+    var peerConnection = null;
+
+    signalingSocket.onmessage = async (event) => {
+        const message = JSON.parse(event.data);
+
+        if (message.type === "answer") {
+            delete message.target_id
+            console.log(message)
+            await peerConnection.setRemoteDescription(message);
+        }
+    };
+
+    async function sendOffer(targetId) {
+        peerConnection.addTransceiver('video', { direction: 'recvonly' });
+        peerConnection.addTransceiver('audio', { direction: 'recvonly' });
+        const offer = await peerConnection.createOffer();
+        await peerConnection.setLocalDescription(offer);
+    
+        await new Promise((resolve) => {
+            if (peerConnection.iceGatheringState === 'complete') {
+                resolve();
+            } else {
+                const checkState = () => {
+                    if (peerConnection.iceGatheringState === 'complete') {
+                        console.log("COMPLETE");
+                        peerConnection.removeEventListener('icegatheringstatechange', checkState);
+                        resolve();
+                    }
+                };
+                peerConnection.addEventListener('icegatheringstatechange', checkState);
+            }
+        });
+    
+        var offer_alt = peerConnection.localDescription;
+        console.log("INITIAL OFFER: " + offer_alt.sdp)
+        console.log(peerConnection.iceGatheringState)
+        signalingSocket.send(JSON.stringify({
+            type: offer_alt.type,
+            target_id: targetId,
+            sdp: offer_alt.sdp,
+        }));
+    }
+
+    React.useEffect(() => {
+        const startup = async () => {
+            var config = {
+                sdpSemantics: 'unified-plan'
+            };
+        
+            // if (document.getElementById('use-stun').checked) {
+            //     config.iceServers = [{ urls: ['stun:stun.l.google.com:19302'] }];
+            // }
+        
+            peerConnection = new RTCPeerConnection(config);
+        
+            peerConnection.addEventListener('track', (evt) => {
+                if (evt.track.kind == 'video') {
+                    document.getElementById('video').srcObject = evt.streams[0];
+                } else {
+                    document.getElementById('audio').srcObject = evt.streams[0];
+                }
+            });
+        
+            // document.getElementById('start').style.display = 'none';
+            await sendOffer('2236119122624');
+            // document.getElementById('stop').style.display = 'inline-block';
+        };
+        startup();
+    }, []);
+
+
     return (
         <>
-            <img 
+            {/* <img 
                 src="http://localhost:5000/video_feed" 
                 alt="Live Stream" 
                 className="camera-display" 
                 onClick={handleOpen} 
                 style={{ cursor: "pointer" }}
-            />
+            /> */}
 
-            <Dialog fullScreen open={open} onClose={handleClose}>
+            <video id="video" autoplay="true" playsinline="true"></video>
+
+            {/* <Dialog fullScreen open={open} onClose={handleClose}>
                 <DialogContent 
                     style={{ 
                         backgroundColor: "black", 
@@ -68,7 +143,7 @@ const LiveStream = ({ camera }) => {
                         <CameraSettings camera={camera} setOpenDialog={setOpen}/>
                     </Box>
                 </DialogContent>
-            </Dialog>
+            </Dialog> */}
         </>
     );
 };
