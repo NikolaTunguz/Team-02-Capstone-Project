28faa3e402bdb218d08caf02c0a9c7b0b3d04ad1
Reni Wu <reniw@nevada.unr.edu>
2025-02-28 17:56:33 -0800

    ui updates

--- Website/web/src/components/LiveStream.jsx
+++ Website/web/src/components/LiveStream.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { Dialog, DialogContent, IconButton, Box } from "@mui/material";
+import { Dialog, DialogContent, IconButton, Box, Tooltip } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
-import CameraSettings from "./CameraSettings"; 
+import CameraSettings from "./CameraSettings";
 import "../App.css";
 
 const LiveStream = ({ camera }) => {
@@ -12,60 +12,62 @@ const LiveStream = ({ camera }) => {
 
     return (
         <>
-            <img 
-                src="http://localhost:5000/video_feed" 
-                alt="Live Stream" 
-                className="camera-display" 
-                onClick={handleOpen} 
+            <img
+                src="http://localhost:5000/video_feed"
+                alt="Live Stream"
+                className="camera-display"
+                onClick={handleOpen}
                 style={{ cursor: "pointer" }}
             />
 
             <Dialog fullScreen open={open} onClose={handleClose}>
-                <DialogContent 
-                    style={{ 
-                        backgroundColor: "black", 
-                        display: "flex", 
-                        justifyContent: "flex-start", 
+                <DialogContent
+                    style={{
+                        backgroundColor: "#dbe6ef",
+                        display: "flex",
+                        justifyContent: "flex-start",
                         alignItems: "center",
                         height: "100vh",
-                        padding: "20px" 
+                        padding: "20px"
                     }}
                 >
-                    <IconButton 
-                        edge="start" 
-                        color="inherit" 
-                        onClick={handleClose} 
-                        aria-label="close"
-                        style={{ position: "absolute", top: 10, right: 10, color: "white" }}
-                    >
-                        <CloseIcon />
-                    </IconButton>
+                    <Tooltip title="Close live stream">
+                        <IconButton
+                            edge="start"
+                            color="inherit"
+                            onClick={handleClose}
+                            aria-label="close"
+                            style={{ position: "absolute", top: 10, right: 10, color: "black" }}
+                        >
+                            <CloseIcon />
+                        </IconButton>
+                    </Tooltip>
 
-                    <Box 
-                        style={{ 
-                            width: "70vw",  
+                    <Box
+                        style={{
+                            width: "70vw",
                             height: "80vh",
-                            border: "3px solid white", 
-                            borderRadius: "4px", 
+                            border: "3px solid white",
+                            borderRadius: "4px",
                             display: "flex",
                             justifyContent: "center",
                             alignItems: "center",
-                            overflow: "hidden" 
+                            overflow: "hidden"
                         }}
                     >
-                        <img 
-                            src="http://localhost:5000/video_feed" 
-                            alt="Live Stream" 
-                            style={{ 
-                                width: "100%", 
-                                height: "100%", 
-                                objectFit: "cover", 
+                        <img
+                            src="http://localhost:5000/video_feed"
+                            alt="Live Stream"
+                            style={{
+                                width: "100%",
+                                height: "100%",
+                                objectFit: "cover",
                             }}
                         />
                     </Box>
 
                     <Box style={{ marginLeft: "20px", display: "flex", alignItems: "center" }}>
-                        <CameraSettings camera={camera} setOpenDialog={setOpen}/>
+                        <CameraSettings camera={camera} setOpenDialog={setOpen} />
                     </Box>
                 </DialogContent>
             </Dialog>
