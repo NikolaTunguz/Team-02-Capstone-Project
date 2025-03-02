52fb4d7901462910032175312b79fb0e44964df9
Reni Wu <reniw@nevada.unr.edu>
2025-03-01 17:04:02 -0800

    create admin user by default

--- Website/api/venv/main.py
+++ Website/api/venv/main.py
@@ -7,7 +7,11 @@ from routes.emergency_contact import emergency_contact_bp
 from routes.cameras import camera_bp
 from routes.notifications import notifications_bp
 from routes.manage_users import manage_users_bp
+from model import User
+from uuid import uuid4
+from flask_bcrypt import Bcrypt
 
+bcrypt = Bcrypt()
 app = Flask(__name__)
 cors = CORS(app, origins=["http://localhost:5173"], supports_credentials=True)
 app.config.from_object(ApplicationConfig)
@@ -17,9 +21,26 @@ app.register_blueprint(camera_bp)
 app.register_blueprint(notifications_bp)
 app.register_blueprint(manage_users_bp)
 
+def create_admin_user():
+    with app.app_context():
+        if not User.query.filter_by(email="admin@admin").first():
+            admin = User(
+                id=uuid4().hex, 
+                phone_number="0000000000",
+                first_name="Admin",
+                last_name="Admin",
+                email="admin@admin",
+                password=bcrypt.generate_password_hash("Password123").decode("utf-8"), 
+                account_type="admin"
+            )
+            db.session.add(admin)
+            db.session.commit()
+            print("Admin user created successfully.")
+
 db.init_app(app)
 with app.app_context():
     db.create_all()
+    create_admin_user()
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
