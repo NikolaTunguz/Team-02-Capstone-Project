4ac1e0bcf23c97ed808502cc9f5fd815e2ec1400
Reni Wu <reniw@nevada.unr.edu>
2025-04-06 16:13:09 -0700

    make notification sounds play from anywhere

--- Website/web/src/App.jsx
+++ Website/web/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { useAuth } from './context/AuthContext';
 import Router from './Router';
 import NavBar from './components/NavBar';
+import NotificationListener from './components/NotificationListener';
 
 const App = () => {
   const { isLoggedIn } = useAuth();
 
   return (
     <>
+      {isLoggedIn && <NotificationListener />}
       {isLoggedIn && <NavBar />}
       <Router />
     </>
