5e1c4567be539feaa100159b06ced97cf29ab788
borne-diego <dborne@unr.edu>
2025-04-28 01:45:14 -0700

    Minor pi updates and webcam.py restructure.

--- Embedded System/api/venv/webcam.py
+++ Embedded System/api/venv/webcam.py
@@ -30,33 +30,41 @@ connections = set()
 
 SIGNALING_SERVER_URI = "ws://seethru-wss.unr.dev"
 
-class TestTrack(VideoStreamTrack):
-    def __init__(self, camera_queue, webrtc_con_person, webrtc_con_package):
+
+DEVICE_ID = 18
+
+
+
+
+
+
+class StandardTrack(VideoStreamTrack):
+    def __init__(self, camera_queue, webrtc_con_standard):
         super().__init__()
         self.camera_queue = camera_queue
-        self.webrtc_con_person = webrtc_con_person
-        self.webrtc_con_package = webrtc_con_package
+        self.webrtc_con_standard = webrtc_con_standard
         self.person_bboxes = []
         self.package_bboxes = []
-        
+       
     async def recv(self):
         img = self.camera_queue.get()
 
-        if(self.webrtc_con_person.poll()):
-            self.person_bboxes = self.webrtc_con_person.recv()
+
+        if(self.webrtc_con_standard.poll()):
+            bboxes, det_type = self.webrtc_con_standard.recv()
+            if(det_type == 'person'):
+                self.person_bboxes = bboxes
+            elif(det_type == 'package'):
+                self.package_bboxes = bboxes
        
-        if(self.webrtc_con_package.poll()):
-            self.package_bboxes = self.webrtc_con_package.recv()
-
-
-        font = cv2.FONT_HERSHEY_SIMPLEX
-
         for box in self.person_bboxes:
             x1,y1,x2,y2 = box
 
 
             img = cv2.rectangle(img, (x1, y1), (x2, y2), (255, 199, 46), 1)
-            img = cv2.putText(img, "person", (x1, y1-10), font, 0.6, (0, 0, 200), 1, cv2.LINE_AA)
+            img = cv2.putText(img, "person", (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 200), 1, cv2.LINE_AA)
+
+
 
 
         for box in self.package_bboxes:
@@ -64,7 +72,9 @@ class TestTrack(VideoStreamTrack):
 
 
             img = cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 1)
-            img = cv2.putText(img, "package", (x1, y1-10), font, 0.6, (0, 255, 255), 1, cv2.LINE_AA)
+            img = cv2.putText(img, "package", (x1, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 255), 1, cv2.LINE_AA)
+
+
 
 
         frame = VideoFrame.from_ndarray(img)
@@ -88,7 +98,6 @@ class ThermalTrack(VideoStreamTrack):
             boxes, det_type = self.webrt_con_thermal.recv()
             if(det_type == 'pistol'):
                 self.pistol_bboxes = boxes
-                print(self.pistol_bboxes)
             elif(det_type == 'fire'):
                 self.fire_bboxes = boxes
                 pass
@@ -118,6 +127,33 @@ class ThermalTrack(VideoStreamTrack):
         frame.time_base = time_base
         return frame
    
+def send_notification(frame, path, message, det_type):
+    date = datetime.now()
+    date = date.strftime("%m/%d/%Y, %H:%M:%S")
+
+
+    cv2.imwrite(path, frame)
+
+
+    with open(path, "rb") as img_file:
+        response = requests.post(
+            "https://seethru.unr.dev/api/database",
+            files={"snapshot": ("event_snap.jpg", img_file, "image/jpeg")},
+            data = {
+                "device_id": DEVICE_ID,
+                "timestamp": date,
+                "message": message,
+                "notif_type": det_type,
+            }
+        )
+
+
+        if response.status_code == 200:
+            print("Notification and snapshot sent successfully")
+        else:
+            print(f"Failed to send notification. Status code: {response.status_code}")
+
+
 def camera_reader(camera_queue, thermal_queue):
     print("loading")
     cap_standard = Picamera2()
@@ -134,7 +170,6 @@ def camera_reader(camera_queue, thermal_queue):
     while(True):
         frame = cap_standard.capture_array()
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        #_, frame = cap_standard.read()
         if not camera_queue.full():
             camera_queue.put_nowait(frame)
        
@@ -145,17 +180,20 @@ def camera_reader(camera_queue, thermal_queue):
             combined = np.concatenate((imdata, thdata), axis=1)
             np.save('localcache/thermal_input.npy', combined)
             thermal_image, thermal_grayscale, thermal_data = model_interface.set_thermal_npy(thermal_npy_path)
-            #print(len(thermal_grayscale), ":", len(model_interface.thermal_grayscale))
             thermal_queue.put_nowait((thermal_image, thermal_grayscale, thermal_data))
 
 
-def image_process(camera_queue, im_pro_con_person, im_pro_con_package):
+
+
+def image_process(camera_queue, im_pro_con_standard):
     print("im_pro started")
     previous_person_notification = 0
-    #previous_package_notification = 0
-    #package_timer = 0
+    previous_package_notification = 0
+    package_timer = 0
     prev_person_detections = deque(maxlen=3)
-    #package_detected = False
+    package_detected = False
+
+
 
 
     while(True):
@@ -163,91 +201,51 @@ def image_process(camera_queue, im_pro_con_person, im_pro_con_package):
         cv2.imwrite("localcache/input_image.jpg", frame)
         model_interface.set_normal_image("localcache/input_image.jpg")
 
+
         #person detection
         person_detected = model_interface.detect_person()
         person_bboxes = model_interface.normal_interface.person_bboxes.cpu().numpy().astype("int")
-        im_pro_con_person.send(person_bboxes)
+
+
+        im_pro_con_standard.send((person_bboxes, "person"))
+
 
         current_time = time.time()
 
 
-        '''
+       
         #every X seconds do package detection
         if (current_time - previous_package_notification > package_timer):
             previous_package_notification = current_time
             package_detected = model_interface.detect_package()
             package_bboxes = model_interface.normal_interface.package_bboxes.cpu().numpy().astype("int")
-            im_pro_con_package.send(package_bboxes)
+            im_pro_con_standard.send((package_bboxes, "package"))
             # Update times as necessary (hours-minutes likely)
             if(package_detected):
-                package_timer = 60
+                package_timer = 30
             else:
                 package_timer = 15
-        '''
+       
    
         #notifications
         print(current_time - previous_person_notification > 60, ":", person_detected, ":", not (True in prev_person_detections))
         if ((current_time - previous_person_notification > 60) and person_detected and not (True in prev_person_detections)):
             previous_person_notification = current_time
-            date = datetime.now()
-            date = date.strftime("%m/%d/%Y, %H:%M:%S")
-
-
-            cv2.imwrite("localcache/event_snap.jpg", frame)
-
-
-            with open("localcache/event_snap.jpg", "rb") as img_file:
-                 response = requests.post(
-                    #"http://127.0.0.1:8080/database",
-                    "https://seethru.unr.dev/api/database",
-                    files={"snapshot": ("event_snap.jpg", img_file, "image/jpeg")},
-                    data = {
-                     "device_id": 18,
-                     "timestamp": date,
-                     "message": "Person detected at camera.",
-                     "notif_type": "person",
-                    }
-                 )
-                 if response.status_code == 200:
-                     print("Notification and snapshot sent successfully")
-                 else:
-                     print(f"Failed to send notification. Status code: {response.status_code}")
+            send_notification(frame, "localcache/event_snap.jpg", "Person detected at camera.", "person")
         prev_person_detections.append(person_detected)
-        '''
+       
         if (package_detected):
             previous_package_notification = current_time
-            date = datetime.now()
-            date = date.strftime("%m/%d/%Y, %H:%M:%S")
-
-            cv2.imwrite("localcache/event_snap.jpg", frame)
-
-
-            with open("localcache/event_snap.jpg", "rb") as img_file:
-                    response = requests.post(
-                    "http://127.0.0.1:8080/database",
-                    files={"snapshot": ("event_snap.jpg", img_file, "image/jpeg")},
-                    data = {
-                        "device_id": 14,
-                        "timestamp": date,
-                        "message": "Package detected at camera.",
-                        "notif_type": "package",
-                    }
-                    )
-                    if response.status_code == 200:
-                        print("Notification and snapshot sent successfully")
-                    else:
-                        print(f"Failed to send notification. Status code: {response.status_code}")
+            send_notification(frame, "localcache/event_snap.jpg", "Package detected at camera.", "package")
             package_detected = False
-            '''
+           
        
-async def on_offer(offer_sdp, target_id, camera_queue, thermal_queue, webrtc_con_person, webrtc_con_package, webrtc_con_thermal):
+async def on_offer(offer_sdp, target_id, camera_queue, thermal_queue, webrtc_con_standard, webrtc_con_thermal):
     offer = RTCSessionDescription(sdp=offer_sdp, type="offer")
     print("offer")
     peer_connection = RTCPeerConnection()
 
-
     connections.add(peer_connection)
-
 
     @peer_connection.on("connectionstatechange")
     async def on_connectionstatechange():
@@ -256,7 +254,7 @@ async def on_offer(offer_sdp, target_id, camera_queue, thermal_queue, webrtc_con
             await peer_connection.close()
             connections.discard(peer_connection)
     print("track add start")
-    peer_connection.addTrack(TestTrack(camera_queue, webrtc_con_person, webrtc_con_package))
+    peer_connection.addTrack(StandardTrack(camera_queue, webrtc_con_standard))
     peer_connection.addTrack(ThermalTrack(thermal_queue, webrtc_con_thermal))
     print("track add finish")
     await peer_connection.setRemoteDescription(offer)
@@ -268,6 +266,7 @@ async def on_offer(offer_sdp, target_id, camera_queue, thermal_queue, webrtc_con
         sdp = peer_connection.localDescription.sdp,
     )
 
+
     signaling_message = {
         "type": answer.type,
         "target_id": target_id,
@@ -276,41 +275,34 @@ async def on_offer(offer_sdp, target_id, camera_queue, thermal_queue, webrtc_con
     await signaling_socket.send(json.dumps(signaling_message))
 
 
-async def handle_signaling_messages(signaling_socket, thermal_queue, camera_queue, webrtc_con_person, webrtc_con_package, webrtc_con_thermal):
+
+
+async def handle_signaling_messages(signaling_socket, thermal_queue, camera_queue, webrtc_con_standard, webrtc_con_thermal):
     async for message in signaling_socket:
         data = json.loads(message)
 
 
+
+
         if data.get("type") == "offer":
-            await on_offer(data["sdp"], data["target_id"], camera_queue, thermal_queue, webrtc_con_person, webrtc_con_package, webrtc_con_thermal)
+            await on_offer(data["sdp"], data["target_id"], camera_queue, thermal_queue, webrtc_con_standard, webrtc_con_thermal)
 
 
-async def connect_to_signaling_server(camera_queue, thermal_queue, webrtc_con_person, webrtc_con_package, webrtc_con_thermal):
+
+
+async def connect_to_signaling_server(camera_queue, thermal_queue, webrtc_con_standard, webrtc_con_thermal):
     global signaling_socket
     signaling_socket = await websockets.connect(SIGNALING_SERVER_URI)
     connection_message = {
         "type": "setup",
-        "id": 18,
+        "id": DEVICE_ID,
     }
     await signaling_socket.send(json.dumps(connection_message))
     print("Connected to signaling server")
 
+    await handle_signaling_messages(signaling_socket, thermal_queue, camera_queue, webrtc_con_standard, webrtc_con_thermal)
 
-    await handle_signaling_messages(signaling_socket, thermal_queue, camera_queue, webrtc_con_person, webrtc_con_package, webrtc_con_thermal)
 
-
-async def main(camera_queue, thermal_queue, webrtc_con_person, webrtc_con_package, webrtc_con_thermal):
-    await connect_to_signaling_server(camera_queue, thermal_queue, webrtc_con_person, webrtc_con_package, webrtc_con_thermal)
-
-async def im_read(camera_queue, thermal_queue):
-    im_reader = aioprocessing.AioProcess(target=camera_reader, args=[camera_queue, thermal_queue])
-    im_reader.start()
-    await im_reader.coro_join()
-
-async def run_im_pro(camera_queue, im_pro_con_person, im_pro_con_package):
-    im_pro = aioprocessing.AioProcess(target=image_process, args=[camera_queue, im_pro_con_person, im_pro_con_package])
-    im_pro.start()
-    await im_pro.coro_join()
 
 
 def thumbnail_process(camera_queue):
@@ -319,6 +311,7 @@ def thumbnail_process(camera_queue):
 
     while True:
         frame = camera_queue.get()
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
         if frame is None or frame.size == 0:
             print("Error: Empty frame")
             continue
@@ -329,13 +322,10 @@ def thumbnail_process(camera_queue):
                 cv2.imwrite("localcache/thumbnail.jpg", frame)
                 with open("localcache/thumbnail.jpg", "rb") as img_file:
                     response = requests.post(
-                        #"http://127.0.0.1:8080/update_thumbnail",
                         "https://seethru.unr.dev/api/update_thumbnail",
                         files={"thumbnail": ("thumbnail.jpg", img_file, "image/jpeg")},
-                        data={"device_id": 14}
+                        data={"device_id": DEVICE_ID}
                     )
-
-
                     if response.status_code == 200:
                         print("Thumbnail updated successfully")
                     else:
@@ -349,6 +339,36 @@ def thumbnail_process(camera_queue):
             last_thumbnail_time = time.time()
 
 
+   
+def thermal_process(thermal_queue, im_pro_con_thermal):
+    while(True):
+        thermal_image, thermal_grayscale, thermal_data = thermal_queue.get()
+       
+        model_interface.set_thermal_grayscale(thermal_grayscale)
+        model_interface.set_thermal_data(thermal_data)
+
+        if model_interface.detect_pistol():
+            print("Pistol")
+            _, bbox, _ = model_interface.detect_and_bound_pistol()
+            bbox_scaled = [box / 384 for box in bbox]
+            im_pro_con_thermal.send((bbox_scaled, 'pistol'))
+            send_notification(thermal_image, "localcache/event_snap_thermal.jpg", "Pistol detected at camera", "pistol")
+        else:
+            print("No pistol")
+            im_pro_con_thermal.send(((0, 0, 0, 0), 'pistol'))
+       
+
+        det, bbox, _ = model_interface.detect_fire()
+        if det:
+            bbox_scaled = ( bbox[0][0] / 256, bbox[0][1] / 192, bbox[0][2] / 256, bbox[0][3] / 192 )
+            im_pro_con_thermal.send((bbox_scaled, 'fire'))
+            send_notification(thermal_image, "localcache/event_snap_thermal.jpg", "Fire detected at camera", "fire")
+            print('Fire')
+        else:
+            im_pro_con_thermal.send(((0, 0, 0, 0), 'fire'))
+            print('No fire')
+
+
 async def run_thumbnail_process(camera_queue):
     thumbnail_proc = aioprocessing.AioProcess(target=thumbnail_process, args=[camera_queue])
     thumbnail_proc.start()
@@ -359,33 +379,22 @@ async def run_thermal_im_pro(thermal_queue, im_pro_con_thermal):
     thermal_pro = aioprocessing.AioProcess(target=thermal_process, args=[thermal_queue, im_pro_con_thermal])
     thermal_pro.start()
     await thermal_pro.coro_join()
-   
-def thermal_process(thermal_queue, im_pro_con_thermal):
-    while(True):
-        _, thermal_grayscale, thermal_data = thermal_queue.get()
-       
-        model_interface.set_thermal_grayscale(thermal_grayscale)
-        model_interface.set_thermal_data(thermal_data)
 
 
-        if model_interface.detect_pistol():
-            print("TRUE")
-            _, bbox, _ = model_interface.detect_and_bound_pistol()
-            print('direct', bbox)
-            bbox_scaled = [box / 384 for box in bbox]
-            im_pro_con_thermal.send((bbox_scaled, 'pistol'))
-        else:
-            im_pro_con_thermal.send(((0, 0, 0, 0), 'pistol'))
-            print("FALSE")
-           
-        det, bbox, _ = model_interface.detect_fire()
-        if det:
-            bbox_scaled = ( bbox[0][0] / 256, bbox[0][1] / 192, bbox[0][2] / 256, bbox[0][3] / 192 )
-            im_pro_con_thermal.send((bbox_scaled, 'fire'))
-            print('Fire')
-        else:
-            im_pro_con_thermal.send(((0, 0, 0, 0), 'fire'))
-            print('No fire')
+async def stream(camera_queue, thermal_queue, webrtc_con_standard, webrtc_con_thermal):
+    await connect_to_signaling_server(camera_queue, thermal_queue, webrtc_con_standard, webrtc_con_thermal)
+
+
+async def im_read(camera_queue, thermal_queue):
+    im_reader = aioprocessing.AioProcess(target=camera_reader, args=[camera_queue, thermal_queue])
+    im_reader.start()
+    await im_reader.coro_join()
+
+
+async def run_im_pro(camera_queue, im_pro_con_standard):
+    im_pro = aioprocessing.AioProcess(target=image_process, args=[camera_queue, im_pro_con_standard])
+    im_pro.start()
+    await im_pro.coro_join()
 
 
 if __name__ == "__main__":
@@ -396,21 +405,20 @@ if __name__ == "__main__":
     camera_queue = aioprocessing.AioQueue(1)
     thermal_queue = aioprocessing.AioQueue(1)
 
-    webrtc_con_person, im_pro_con_person = aioprocessing.AioPipe(duplex=False)
-    webrtc_con_package, im_pro_con_package = aioprocessing.AioPipe(duplex=False)
+
+    webrtc_con_standard, im_pro_con_standard = aioprocessing.AioPipe(duplex=False)
     webrtc_con_thermal, im_pro_con_thermal = aioprocessing.AioPipe(duplex=False)
 
 
     tasks = [
         asyncio.ensure_future(im_read(camera_queue, thermal_queue)),
-        asyncio.ensure_future(main(camera_queue, thermal_queue, webrtc_con_person, webrtc_con_package, webrtc_con_thermal)),
-        asyncio.ensure_future(run_im_pro(camera_queue, im_pro_con_person, im_pro_con_package)),
+        asyncio.ensure_future(stream(camera_queue, thermal_queue, webrtc_con_standard, webrtc_con_thermal)),
+        asyncio.ensure_future(run_im_pro(camera_queue, im_pro_con_standard)),
         asyncio.ensure_future(run_thermal_im_pro(thermal_queue, im_pro_con_thermal)),
-        #asyncio.ensure_future(run_thumbnail_process(camera_queue)),
+        asyncio.ensure_future(run_thumbnail_process(camera_queue)),
     ]
     try:
         loop.run_until_complete(asyncio.wait(tasks))
     except:
         os.system('taskkill -f -im python*' if os.name == 'nt' else 'pkill -9 python')
     loop.close()
-
